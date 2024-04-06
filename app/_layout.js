362948cc3ed005857a58053f20e78fdcec93c7f2
shafqat007 <shafqatnawazchy@gmail.com>
2024-04-06 15:25:54 +0600

    tailwind implemented

---
+++ app/_layout.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {Slot} from 'expo-router';
+import "../global.css"
+
+export default function _layout() {
+  return (
+    <View>
+   <Slot/>
+    </View>
+  )
+}
--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{paddingTop:40}} className="bg-red-200">
+      <Text className="text-3xl text-center">Home</Text>
     </View>
   )
 }
